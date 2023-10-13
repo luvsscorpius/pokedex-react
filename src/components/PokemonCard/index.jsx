f99c58692c9d08d5ef720da1380757bfe1912a5b
and <andersonvittor1801@gmail.com>
2023-10-13 17:51:02 -0300

    feat(cards n navbar): adding navbar n modifying cards

--- src/components/PokemonCard/index.jsx
+++ src/components/PokemonCard/index.jsx
@@ -18,14 +18,14 @@ export default function MediaCard() {
                 <Typography gutterBottom variant="h5" component="div">
                     Lizard
                 </Typography>
-                <Typography variant="body2" color="text.secondary">
+                {/* <Typography variant="body2" color="text.secondary">
                     Lizards are a widespread group of squamate reptiles, with over 6,000
                     species, ranging across all continents except Antarctica
-                </Typography>
+                </Typography> */}
             </CardContent>
             <CardActions>
-                <Button size="small">Share</Button>
-                <Button size="small">Learn More</Button>
+                {/* <Button size="small">Share</Button>
+                <Button size="small">Learn More</Button> */}
             </CardActions>
         </Card>
     );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,32 @@
 import React from 'react'
 import Navbar from '../components/NavBar/index.jsx'
+import PokemonCard from '../components/PokemonCard/index.jsx'
+import { Container } from '@mui/material'
+import { Grid } from '@mui/material'
 
 export const Home = () => {
     return (
         <div>
             <Navbar />
+            <Container maxWidth="false">
+                <Grid container>
+                    <Grid item xs={3}>
+                        <PokemonCard />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <PokemonCard />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <PokemonCard />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <PokemonCard />
+                    </Grid>
+                    <Grid item xs={3}>
+                        <PokemonCard />
+                    </Grid>
+                </Grid>
+            </Container>
         </div>
     )
 }
