95c2d0cc47f004888f1fd8d0e3a17d9a6b914f65
and <andersonvittor1801@gmail.com>
2023-10-13 16:21:08 -0300

    feat(initial commit): starting a new repo

---
+++ src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Navbar from '../components/NavBar/index.jsx'
+
+export const Home = () => {
+    return (
+        <div>
+            <Navbar />
+        </div>
+    )
+}
